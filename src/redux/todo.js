fef7ff7658f42e8312ecd3313b0edeb02b6b4075
hyemin065 <p62517@gmail.com>
2021-02-17 18:25:04 +0900

    todo completed

--- src/redux/todo.js
+++ src/redux/todo.js
@@ -1,21 +1,42 @@
 const ADD_TODO = "add_todo";
 const DEL_TODO = "del_todo";
+const DONE_TODO = "done_todo";
+const MODIFY_TODO = "modify_todo";
+const UPDATE_TODO = "update_todo";
 
 export const addTodo = (text) => ({
     type:ADD_TODO,
     todos:{
         id: Math.floor(Math.random()*10000000),
-        text
+        text,
+        done: false,
+        modify: false
     }
 })
 
 export const delTodo = (id) => ({
     type:DEL_TODO,
     id
-    
+})
+
+export const doneTodo = id => ({
+    type: DONE_TODO,
+    id
+})
+
+export const modifyTodo = id =>({
+    type: MODIFY_TODO,
+    id
+})
+
+export const updateTodo = (id,text) =>({
+    type:UPDATE_TODO,
+    id,
+    text
 })
 
 const todoState = [];
+
 
 
 export default function todo(state = todoState, action){
@@ -26,6 +47,28 @@ export default function todo(state = todoState, action){
             return state.filter(todo => {
                 return todo.id !== action.id               
             });
+        case DONE_TODO:
+            return state.map(todo => {
+                if(todo.id === action.id){
+                    todo.done = !todo.done;
+                }
+                return todo;
+            })
+            case MODIFY_TODO:
+                return state.map(todo =>{
+                    if(todo.id === action.id){
+                        todo.modify = true;
+                    }
+                    return todo;
+                })
+            case UPDATE_TODO:
+                return state.map(todo=>{
+                    if(todo.id === action.id){
+                        todo.modify = false;
+                        todo.text = action.text
+                    }
+                    return todo;
+                })
         default:
             return state;
     
--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import {addTodo, delTodo} from '../redux/todo';
+import {addTodo, delTodo, doneTodo, modifyTodo, updateTodo} from '../redux/todo';
 
 function Todo(){
     const [text, setText] = useState("");
+    const [inputText, setInputText] = useState("");
 
     function getText(e){
-        const {value} =e.target;
-        setText(value);
+        if(e.target.name === "todo"){
+            const {value} =e.target;
+            setText(value);
+        }else if(e.target.name === "edit"){
+            const {value} = e.target;
+            setInputText(value); 
+        }
     }
 
     const todos = useSelector(state=> state);
@@ -20,18 +26,33 @@ function Todo(){
     const delTodos = (id) =>{
         dispatch(delTodo(id));
     }
+    const doneTodos = id => {
+        dispatch(doneTodo(id));
+    }
+    const modifyTodos = (id,text) => {
+        setInputText(text);
+        dispatch(modifyTodo(id));
+    }
+
+    const updateTodos = (id) => {
+        dispatch(updateTodo(id, inputText));
+    }
 
 
     return(
         <>
             <form onSubmit={addTodos}>
-                <input type="text" onChange={getText} value={text}/>
+                <input type="text" name="todo" onChange={getText} value={text}/>
                 <button type="submit">제출</button>
             </form>
             <ul>
                 {
                     todos ? todos.map(todo =>  {
-                       return (<li id={todo.id} key={todo.id}>{todo.text}<button onClick={()=>delTodos(todo.id)}>x</button></li> )
+                        if(todo.modify){
+                            return (<span key={todo.id}><input type="text" name="edit" value={inputText} onChange={getText}/><button onClick={() => updateTodos(todo.id)}>완료</button></span>)
+                        }else{
+                            return (<li id={todo.id} key={todo.id}>{todo.text}<button onClick={()=>modifyTodos(todo.id,todo.text)}>수정</button><button onClick={()=>delTodos(todo.id)}>x</button><button onClick={() => doneTodos(todo.id)}>Done</button>{todo.done ? "done" : "false"}</li> )
+                        }
                     }) : null
                 }
 
